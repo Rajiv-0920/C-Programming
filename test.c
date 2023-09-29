8a5bf733d5c5bc5c188a179642f8f93a8bc89cf3
Rajiv Kumar <kumarrajiv0920@gmail.com>
2023-09-29 20:18:36 +0530

    initial changes

--- test.c
+++ test.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    int arr[100];
-    int length, i, position, value;
-    
-    printf("Enter length of the array: ");
-    scanf("%d", &length);
+	int arr[100];
+	int length, i, j, temp;
+	
+	printf("Enter length of the array: ");
+	scanf("%d", &length);
 
-    printf("Enter %d elements: ", length);
-    for(i = 0; i < length; i++){
-        scanf("%d", &arr[i]);
-    }
+	printf("Enter %d elements: ", length);
+	for(i = 0; i < length; i++){
+		scanf("%d", &arr[i]);
+	}
 
-    printf("Enter the position: ");
-    scanf("%d", &position);
-    printf("Enter the value: ");
-    scanf("%d", &value);
 
-    if(position < 1 || position > length + 1){
-        printf("Enter Valid position..!");
-        return 0;
-    }
-
-    for(i = length; i >= position; i--){
-        arr[i] = arr[i - 1];
-    }
-    length++;
-    arr[position - 1] = value;
-
-    for(i = 0; i < length; i++){
-        printf("%d ", arr[i]);
-    }
-    return 0;
+	for(i = 0; i < length; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	return 0;
 }
