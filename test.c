8af2a8a5b82d73cf81f805dda8a8438dbd967988
Rajiv Kumar <kumarrajiv0920@gmail.com>
2023-10-13 17:56:56 +0530

    initial changes

--- test.c
+++ test.c
@@ -7,18 +7,22 @@ int main(){
     scanf("%d", &n);
 
     for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n - i + 1; j++){
+            printf(" ");
+        }
+        for(int j = 1; j <= i + (i - 1); j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf(" ");
+        }
         for(int j = 1; j <= 2 * (n - i) + 1; j++){
-            printf("* ");
+            printf("*");
         }
         printf("\n");
     }
-
-    for(int i = 1; i <= n - 1; i++){
-        for(int j = 1; j <= (2 * i) + 1; j++){
-            printf("* ");
-        }
-        printf("\n");
-    }
-
     return 0;
 }
